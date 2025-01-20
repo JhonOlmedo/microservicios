93d409a9be6d1c264444aa23b4fc605e6cbb01e2
JhonOlmedo <jhonolmedo52@gmail.com>
2025-01-20 04:45:02 -0500

    Terminado

--- microservices-auth/auth-service/src/index.js
+++ microservices-auth/auth-service/src/index.js
@@ -1,15 +1,15 @@
-// product-service/src/index.js
+// auth-service/src/index.js
 const express = require('express');
 const mongoose = require('mongoose');
-const productRoutes = require('./routes/products');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
-app.use('/products', productRoutes);
+app.use('/auth', authRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Product service running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Auth service running on port ${PORT}`));
